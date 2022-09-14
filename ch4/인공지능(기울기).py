381fb610a79bd0caedc1632b04443bb7cd5fa9cf
Jeuk Park <terryf6188@gmail.com>
2022-09-14 14:26:47 +0900

    first commit

---
+++ ch4/인공지능(기울기).py
@@ -0,0 +1,189 @@
+import numpy as np
+
+
+def numerical_gradient(f, x):  #기울기 구현
+    h = 1e-4
+    grad = np.zeros_like(x)  #x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+    return grad
+
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):  #f는 최적화하려는 함수, init_x는 초깃값, lr은 learning rate, step_num은 경사법에 따른 반복 횟수
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr*grad
+    return x
+
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+
+#학습률 lr이 너무 큰 예
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=10.0, step_num=100)) #학습률이 너무 크면 큰 값으로 발산해버림
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100)) #학습률이 너무 작으면 거의 갱신되지 않은 채 끝나버림
+#그러므로 학습률을 적절히 설정해야함
+
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from functions import softmax, cross_entropy_error
+from gradient import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+
+net = simpleNet()
+print(net.W)
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+print(p)
+print(np.argmax(p))
+t = np.array([0, 0, 1])
+print(net.loss(x, t))
+
+
+def f(W):
+    return net.loss(x, t)
+
+
+dW = numerical_gradient(f, net.W)
+print(dW)
+
+f = lambda w: net.loss(x, t)
+dW = numerical_gradient(f, net.W)
+
+import sys, os
+sys.path.append(os.pardir)
+from functions import *
+from gradient import numerical_gradient
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t)/float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+#params는 신경망의 매개변수를 보관하는 딕셔너리 변수(인스턴스 변수)
+#params['W1']은 1번째 층의 가중치, params['b1']은 1번째 층의 편향
+#params['W2']은 2번째 층의 가중치, params['b2']은 2번째 층의 편향
+#grads는 기울기 보관하는 딕셔너리 변수(numerical_gradient()메서드의 반환 값)
+#grads['W1']은 1번째 층의 가중치의 기울기, grads['b1']은 1번째 층의 편향의 기울기
+#grads['W2']은 2번째 층의 가중치의 기울기, grads['b2']은 2번째 층의 편향의 기울기
+
+net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+print(net.params['W1'].shape)
+print(net.params['b1'].shape)
+print(net.params['W2'].shape)
+print(net.params['b2'].shape)
+x = np.random.rand(100, 784)
+y = net.predict(x)
+
+x = np.random.rand(100, 784)
+t = np.random.rand(100, 10)
+
+grads = net.numerical_gradient(x, t)
+print(grads['W1'].shape)
+print(grads['b1'].shape)
+print(grads['W2'].shape)
+print(grads['b2'].shape)
+
+#미니배치 학습 구현하기
+import numpy as np
+from mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+
+#하이퍼파라미터
+iters_num = 10000 #반복 횟수
+train_size = x_train.shape[0]
+batch_size = 100 #미니배치 크기
+learning_rate = 0.1
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+iter_per_epoch = max(train_size / batch_size, 1)
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    #기울기 계산
+    grad = network.numerical_gradient(x_batch, t_batch)
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc |" + str(train_acc) + ',' + str(test_acc))
+
