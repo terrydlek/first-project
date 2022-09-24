756c5e14c2302a0c9cb7a790c7a45fb0b49f5c5e
Jeuk Park <terryf6188@gmail.com>
2022-09-24 17:38:02 +0900

    first commit

--- 연습장/fah_converter.py
+++ 연습장/fah_converter.py
@@ -1,3 +1,11 @@
-graph = [[] for i in range(5 + 1)]
-vi = [False, False, False, False]
-print(not vi[0])
+import heapq
+a = []
+
+heapq.heappush(a, 1)
+heapq.heappush(a, 4)
+heapq.heappush(a, 5)
+heapq.heappush(a, 3)
+heapq.heappush(a, 8)
+heapq.heappush(a, 7)
+
+print(a)
