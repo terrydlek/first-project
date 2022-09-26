6d92b7099ae18b617c3610d871b7ab81a9740187
Jeuk Park <terryf6188@gmail.com>
2022-09-26 13:18:00 +0900

    first commit

--- 연습장/개선된 데이크스트라 알고리즘 연습.py
+++ 연습장/개선된 데이크스트라 알고리즘 연습.py
@@ -1,10 +1,9 @@
-import sys
 import heapq
-INF = int(1e9)
+import sys
 input = sys.stdin.readline
+INF = int(1e9)
 n, m = map(int, input().split())
 start = int(input())
-
 graph = [[] for i in range(n + 1)]
 distance = [INF] * (n + 1)
 
@@ -14,21 +13,20 @@ for _ in range(m):
 
 def dijkstra(start):
     q = []
-    distance[start] = 0
     heapq.heappush(q, (0, start))
+    distance[start] = 0
     while q:
         dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+        cost = dist + distance[start]
+        if dist > distance[now]:
             continue
         for i in graph[now]:
-            cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
 
 dijkstra(start)
-
 for i in range(1, n + 1):
     if distance[i] == INF:
         print("무한대")
