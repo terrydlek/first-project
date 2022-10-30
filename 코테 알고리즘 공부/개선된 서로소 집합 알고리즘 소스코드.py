cf0fc6db9dc8c693e37c898c479f483cde104528
Jeuk Park <terryf6188@gmail.com>
2022-10-30 14:35:28 +0900

    first commit

--- 코테 알고리즘 공부/개선된 서로소 집합 알고리즘 소스코드.py
+++ 코테 알고리즘 공부/개선된 서로소 집합 알고리즘 소스코드.py
@@ -1,10 +1,11 @@
+# 기본적인 서로소 집합 알고리즘 소스코드
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 
-def union_parent(parent, a, b):
+def union(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a < b:
@@ -21,14 +22,14 @@ for i in range(1, v + 1):
 
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parent, a, b)
+    union(parent, a, b)
 
-print("각 원소가 속한 집합: ", end='')
-for i in range(1, v + 1):
+print("각 원소가 속한 집합 ", end='')
+for i in range(v + 1):
     print(find_parent(parent, i), end=' ')
 
 print()
 
-print('부모 테이블: ', end='')
+print("부모 테이블: ", end='')
 for i in range(1, v + 1):
     print(parent[i], end=' ')
