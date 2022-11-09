bf3b1ca3f1071fe9c40b1618fcfec88537b45e09
Jeuk Park <terryf6188@gmail.com>
2022-11-09 13:58:01 +0900

    past question

--- 연습장/알고리즘 복습.py
+++ 연습장/알고리즘 복습.py
@@ -1,13 +1,12 @@
 n = int(input())
 data = list(map(int, input().split()))
 data.sort()
-result = 0
-count = 0
 
-for i in data:
-    count += 1
-    if count >= i:
-        result += 1
-        count = 0
+target = 1
 
-print(result)
+for x in data:
+    if target < x:
+        break
+    target += x
+
+print(target)
