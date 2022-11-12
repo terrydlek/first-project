a1d5ff6404ec41cd369ae691fd255e80bd5d5cba
Jeuk Park <terryf6188@gmail.com>
2022-11-12 14:47:54 +0900

    past question

--- 코테 알고리즘 공부/럭키 스트레이트.py
+++ 코테 알고리즘 공부/럭키 스트레이트.py
@@ -8,7 +8,6 @@
 # 내가 쓴 코드
 n = int(input())
 string = str(n)
-
 left = string[:len(string) // 2]
 right = string[len(string) // 2:]
 
@@ -26,3 +25,20 @@ else:
     print("READY")
 
 # 다른 코드
+'''n = input()
+length = len(n)
+summary = 0
+
+# 왼쪽 부분의 자릿수 합 더하기
+for i in range(length // 2):
+    summary += int(n[i])
+
+# 오른쪽 부분의 자릿수 합 빼기
+for i in range(length // 2, length):
+    summary -= int(n[i])
+# 왼쪽 부분과 오른쪽 부분의 자릿수 합이 동일한지 검사
+if summary == 0:
+    print("LUCKY")
+else:
+    print("READY")
+'''
