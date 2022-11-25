cbe6747a48effb210e7e43386a1d323ff66304e7
Jeuk Park <terryf6188@gmail.com>
2022-11-25 13:09:15 +0900

    first commit

--- 연습장/실험.py
+++ 연습장/실험.py
@@ -1,6 +1,6 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-for i in a:
-    if i % 3 == 0 or i % 2 == 0:
-        print(i, "yes")
-    else:
-        print(i, "no")
+a = ["ceqwcqw park"]
+b, c = a[0].split(" ")
+print(b, c)
+
+ab = [1,2,1,2,1]
+print(ab.count(1))
