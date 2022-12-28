b9b09c23562408aa6f5cbe3d7690e4072e034d7f
Jeuk Park <terryf6188@gmail.com>
2022-12-28 15:38:59 +0900

    programmers

---
+++ 코테 알고리즘 공부/두 개 뽑아서 더하기(programmers).py
@@ -0,0 +1,18 @@
+'''
+정수 배열 numbers가 주어집니다.
+numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를
+배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+'''
+numbers = list(map(int, input().split()))
+
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if (numbers[i] + numbers[j]) not in answer:
+                answer.append(numbers[i] + numbers[j])
+    return sorted(answer)
+
+
+print(solution(numbers))
