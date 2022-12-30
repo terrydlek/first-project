3982800b7e06c84b30c1a84afb91bce58d6a3fa9
Jeuk Park <terryf6188@gmail.com>
2022-12-30 14:37:19 +0900

    programmers

---
+++ 코테 알고리즘 공부/실패율(programmers).py
@@ -0,0 +1,32 @@
+'''
+슈퍼 게임 개발자 오렐리는 큰 고민에 빠졌다. 그녀가 만든 프랜즈 오천성이 대성공을 거뒀지만, 요즘 신규 사용자의 수가 급감한 것이다.
+원인은 신규 사용자와 기존 사용자 사이에 스테이지 차이가 너무 큰 것이 문제였다.
+이 문제를 어떻게 할까 고민 한 그녀는 동적으로 게임 시간을 늘려서 난이도를 조절하기로 했다.
+역시 슈퍼 개발자라 대부분의 로직은 쉽게 구현했지만, 실패율을 구하는 부분에서 위기에 빠지고 말았다. 오렐리를 위해 실패율을 구하는 코드를 완성하라.
+실패율은 다음과 같이 정의한다.
+스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때,
+실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라.
+'''
+N = int(input())
+stages = list(map(int, input().split()))
+
+
+def solution(N, stages):
+    answer = []
+    li = [0] * (N + 2)
+    for i in range(1, N + 2):
+        li[i] = stages.count(i)
+    for i in range(1, len(li) - 1):
+        if li[i] == 0:
+            answer.append(0)
+        else:
+            answer.append(li[i] / sum(li[i:]))
+    result = []
+    while answer != [-1] * len(answer):
+        result.append(answer.index(max(answer)) + 1)
+        answer[answer.index(max(answer))] = -1
+    return result
+
+
+solution(N, stages)
