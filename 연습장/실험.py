8b2c095bd34ed7691f410f632ab06b7922f6f099
Jeuk Park <terryf6188@gmail.com>
2023-01-08 01:33:07 +0900

    programmers

--- 연습장/실험.py
+++ 연습장/실험.py
@@ -1,5 +1,27 @@
 from collections import deque
-a = [1, 2, 3]
-a = deque(a)
-deque.pop(a)
-print(a)
+
+
+def solution(n=8, a=4, b=7):
+    answer = 1
+    li = deque([i for i in range(1, n + 1)])
+    while len(li) > 2:
+        for i in range(0, len(li), 2):
+            if li[0] == a and li[1] == b:
+                return answer
+            elif li[0] == b and li[1] == a:
+                return answer
+            if li[0] == a or li[1] == a:
+                li.append(a)
+            elif li[0] == b or li[1] == b:
+                li.append(b)
+            else:
+                li.append(li[0])
+            deque.popleft(li)
+            deque.popleft(li)
+        answer += 1
+        print(li, answer)
+        print("==================")
+    return answer
+
+
+print(solution(8, 4, 7))
---
+++ 연습장/알고리즘 복습.py
@@ -0,0 +1,3 @@
+from collections import deque
+li = deque([1,2,3,4])
+print(li[0])
