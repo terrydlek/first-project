c80db7980b271bf8d4ce6c46326b4cdee3b4c91a
Jeuk Park <terryf6188@gmail.com>
2023-04-04 16:30:48 +0900

    programmers

--- 코테 알고리즘 공부/점 찍기(programmers).py
+++ 코테 알고리즘 공부/점 찍기(programmers).py
@@ -15,65 +15,14 @@ k, d = map(int, input().split())
 def solution(k, d):
     answer = 0
     li = [i for i in range(0, d + 1, k)]
-    li.reverse()
-    for i in li:
-        if (i**2) * 2 <= d**2:
-            answer += (i//k + 1) ** 2
-            return answer
+    while li:
+        i = li.pop()
+        if i**2 + i**2 > d**2:
+            answer += (int((d**2 - i**2)**0.5)//k + 1) * 2
         else:
-            for j in li:
-                if (i**2 + j**2) <= d**2:
-                    answer += (len(li) - li.index(j)) * 2
-                    break
-    return answer
+            li.append(i)
+            break
+    return answer + len(li)**2
 
 
 print(solution(k, d))
-
-from collections import deque
-def solution(k, d):
-    def cal(y, x, k, d):
-        q = deque()
-        ch = []
-        q.append((y, x))
-        criteria = d**2
-        count = 0
-        while q:
-            chy, chx = q.popleft()
-            if (chy, chx) not in ch:
-                count += 1
-            ch.append((chy, chx))
-            if (chy + k)**2 + chx**2 <= criteria:
-                q.append((chy + k, x))
-            if chy**2 + (chx + k)**2 <= criteria:
-                q.append((chy, chx + k))
-            if (chy + k)**2 + (chx + k)**2 <= criteria:
-                q.append((chy + k, chx + k))
-        return count
-    return cal(0, 0, k, d)
-
-
-import math
-def solution(k, d):
-    answer = 0
-    def cal(y, x, k, d):
-        visited = [["O"] * (d + 1) for _ in range(d + 1)]
-        criteria = math.pow(d, 2)
-        count = 1
-        positions = [(y, x)]
-        while positions:
-            chy, chx = positions.pop(0)
-            if chy + k <= d and math.pow(chy + k, 2) + math.pow(chx, 2) <= criteria and visited[chy + k][chx] != "V":
-                count += 1
-                visited[chy + k][chx] = "V"
-                positions.append((chy + k, x))
-            if chx + k <= d and math.pow(chy, 2) + math.pow(chx + k, 2) <= criteria and visited[chy][chx + k] != "V":
-                count += 1
-                visited[chy][chx + k] = "V"
-                positions.append((chy, chx + k))
-            if chy + k <= d and chx + k <= d and math.pow(chy + k, 2) + math.pow(chx + k, 2) <= criteria and visited[chy + k][chx + k] != "V":
-                count += 1
-                visited[chy + k][chx + k] = "V"
-                positions.append((chy + k, chx + k))
-        return count
-    return cal(0, 0, k, d)
