4954072736668662b15df56fc2a1aec79b8bd68c
Jeuk Park <terryf6188@gmail.com>
2023-07-16 00:57:29 +0900

    data processing

--- 전산 프로젝트/악성코드,바이러스 탐지 프로그램 만들기.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = '5D7F30B9145B41DD888A72A24C9C9A7AA671995FE0EF4F1189AAFA20179EF4E4'
-
-
-params = {'api_key': api_key, 'hash': '111BFB224BAADB8016'}
-response = requests.get('https://public.api.malwares.com/api/v22/file/upload', params=params)
-json_response = response.json()
-print(json_response)
